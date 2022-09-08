f9d42d8f3633653ddf0b8eab31e45f90cb6e16d9
jensitus <jens@service-b.org>
2022-09-08 18:03:46 +0200

    underscore

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,13 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon AWS
       :provider => 'AWS',
-      :aws_access_key_id => ENV["S3_DO_ACCESS_KEY"], # Rails.application.secrets.s3_access_key,      # ,ENV['S3_ACCESS_KEY']
-      :aws_secret_access_key => ENV["S3_DO_SECRECT_KEY"], # Rails.application.secrets.s3_secret_key   # ENV['S3_SECRECT_KEY']
-      :region => 'fra1',
-      :host => 'fra1.digitaloceanspaces.com',
-      :endpoint => 'https://fra1.digitaloceanspaces.com'
+      :aws_access_key_id => ENV["S3_ACCESS_KEY"], # Rails.application.secrets.s3_access_key,      # ,ENV['S3_ACCESS_KEY']
+      :aws_secret_access_key => ENV["S3_SECRET_KEY"], # Rails.application.secrets.s3_secret_key   # ENV['S3_SECRECT_KEY']
+      :region => 'eu-central-1'
+      # :host => 'fra1.digitaloceanspaces.com',
+      # :endpoint => 'https://fra1.digitaloceanspaces.com'
     }
-    config.fog_directory = ENV["S3_DO_BUCKET"], # Rails.application.secrets.s3_bucket              # ENV['S3_BUCKET']
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+    config.fog_directory = ENV["S3_BUCKET"] # Rails.application.secrets.s3_bucket              # ENV['S3_BUCKET']
+    # config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
   end
 end
