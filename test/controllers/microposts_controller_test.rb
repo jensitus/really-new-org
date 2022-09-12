7f06168ad63e6ecb977ebe3b4a66a7d9b346650b
jensitus <jens@service-b.org>
2022-09-12 23:02:33 +0200

    test sign_in

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,37 +1,46 @@
 require "test_helper"
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    sign_in users(:one)
     @micropost = microposts(:one)
   end
+
+  # test 'should sign in' do
+  #   get '/users/sign_in'
+  #   sign_in users(:one)
+  #   post user_session_url
+  #   assert_response :success
+  # end
 
   test "should get index" do
     get microposts_url
     assert_response :success
   end
 
-  # test "should get new" do
-  #   get new_micropost_url
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get new_micropost_url
+    assert_response :success
+  end
 
-  # test "should create micropost" do
-  #   # assert_difference("Micropost.count") do
-  #   post "/post", params: { micropost: { content: "@micropost.content", user_id: 1 } }
-  #   # end
-  #
-  #   assert_redirected_to micropost_url(Micropost.last)
-  # end
+  test "should create micropost" do
+    assert_difference("Micropost.count") do
+      post "/post", params: { micropost: { content: "@micropost.content", user_id: 1 } }
+    end
+    assert_redirected_to micropost_url(Micropost.last)
+  end
 
   test "should show micropost" do
     get micropost_url(@micropost)
     assert_response :success
   end
 
-  # test "should get edit" do
-  #   get edit_micropost_url(@micropost)
-  #   assert_response :success
-  # end
+  test "should get edit" do
+    get edit_micropost_url(@micropost)
+    assert_response :success
+  end
 
   test "should update micropost" do
     patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
